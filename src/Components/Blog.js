226d3f0078cd7027a0f3902893338b492394c057
arijitmandal10 <arijitmandal999@gmail.com>
2023-04-26 00:44:21 +0530

    second update

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,4 +1,8 @@
-import { useState, useEffect, useRef} from "react";
+import { useState, useEffect, useRef } from "react";
+import { db } from "../firebaseInit";
+import { collection, addDoc, getDocs, onSnapshot, deleteDoc, doc } from "firebase/firestore";
+
+// Add a new document with a generated id.
 
 
 
@@ -12,33 +16,83 @@ export default function Blog() {
     const [blogs, setBlogs] = useState([]);
     const titleRef = useRef(null);
 
-    useEffect(()=>{
+    useEffect(() => {
         titleRef.current.focus();
 
-    },[])
+    }, [])
 
-    useEffect(()=>{
 
-        if(blogs.length && blogs[0].title){
+    useEffect(() => {
+
+        // async function fetchData() {
+
+        //     const Snapshot = await getDocs(collection(db, "Blogs"));
+        //     const blogs = Snapshot.docs.map((data) => {
+        //         return {
+
+        //             id: data.id,
+        //             ...data.data()
+        //         }
+        //     })
+
+        //     console.log(blogs);
+        //     setBlogs(blogs);
+        // }
+        // fetchData();
+
+        const unsub = onSnapshot(collection(db, "Blogs"), (Snapshot) => {
+
+            const blogs = Snapshot.docs.map((data) => {
+                return {
+
+                    id: data.id,
+                    ...data.data()
+                }
+            });
+
+            setBlogs(blogs);
+        })
+
+    }, [])
+
+    useEffect(() => {
+
+        if (blogs.length && blogs[0].title) {
             document.title = blogs[0].title;
         }
-        else{
-            document.title="Blogs App"
+        else {
+            document.title = "Blogs App"
         }
     })
 
 
     //Passing the synthetic event as argument to stop refreshing the page on submit
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
 
         e.preventDefault();
-        setBlogs([{ title: formData.title, content: formData.content }, ...blogs]);
+        // setBlogs([{ title: formData.title, content: formData.content }, ...blogs]);
+
+
+        // FIREBASE
+
+        const docRef = await addDoc(collection(db, "Blogs"), {
+            title: formData.title,
+            content: formData.content
+        });
+
+        // console.log("Document written with ID: ", docRef.id);
+
+
         setFormData({ title: "", content: "" })
         titleRef.current.focus();
     }
 
-    function handleDelete(i){
-      setBlogs(blogs.filter((blog, index)=>i!==index))
+    async function handleDelete(id) {
+
+        const docRef = doc(db, "Blogs", id);
+        await deleteDoc(docRef);
+
+        // setBlogs(blogs.filter((blog, index) => i !== index))
 
     }
 
@@ -84,24 +138,26 @@ export default function Blog() {
 
                     <div className="Blog">
 
-                        <div className="contentmain">
-                            <ul>
-                           <li><h3>{blog.title}</h3></li> 
-                           <li><p>{blog.content}</p></li>
-                            </ul>
-                        </div>
-                        <button onClick={()=>handleDelete(i)}>X</button>
-                    </div>
-                    {/* </div> */}
-                    <hr />
-                </>
-            ))}
 
 
+                        <h3 id="title">{blog.title}</h3>
+                        <hr />
+                        <p style={{ fontSize: "larger", fontFamily: "cursive"}}>{blog.content}</p>
 
-        </>
-    )
+
+                    <div className="del" ><button id="del-col" onClick={() => handleDelete(blog.id)}>X</button></div>
+                </div >
+                {/* </div> */ }
+                < hr />
+                    </>
+    ))
 }
+
+
+
+            </>
+        )
+    }
 
 //Row component to introduce a new row section in the form
 function Row(props) {
@@ -114,3 +170,7 @@ function Row(props) {
         </>
     )
 }
+
+
+
+
